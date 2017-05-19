233308b88a9322768d2a16f9178614640c7c021b
Alexander Dodatko <alexander.dodatko@dataart.com>
2017-05-19 11:22:53 +0300

    [multiselect] separate directory for picker

---
+++ nMessenger/Source/Messenger/Components/MultiselectPicker/MultiselectPickerLocalizer.swift
@@ -0,0 +1,23 @@
+//
+//  MultiselectPickerLocalizer.swift
+//  nMessenger
+//
+//  Created by Alexander Dodatko on 5/19/17.
+//  Copyright © 2017 Ebay Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public struct MultiselectPickerLocalizer
+{
+    public var pickerNameForNavBar      : String?
+    public var backButtonName           : String?
+    
+    public var checkmarkIcon            : UIImage?
+    public var cameraCellBackgroundImage: UIImage?
+    
+    public var cellViewNibName          : String?
+    public var cellViewBundle           : Bundle?
+}
+
